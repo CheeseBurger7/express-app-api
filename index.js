8daa3bbcfae3d1568b4307c9928844d0dd6d4c0d
Meet Rachhadia <meetrachhadia910@gmail.com>
2021-05-06 21:46:30 -0400

    First Commit

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import data from './data/data.json';
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+app.use('/images', express.static('images'));
+
+app.get('/', (req, res)=>{
+    res.json(data);
+});
+
+app.get('/item/:id', (req, res)=>{
+    let user = Number(req.params.id);
+});
+
+app.post('/newItem', (req, res)=>{
+    res.send(`a post request with /newItem route on port ${PORT}`);
+});
+
+app.post('/item', (req, res)=>{
+    res.send(`a put request with /item route on port ${PORT}`);
+});
+
+app.delete('/item', (req, res)=>{
+    res.send(`a delete request with /item route on port ${PORT}`);
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server running in port ${PORT}`);
+    // console.log(data);
+});
